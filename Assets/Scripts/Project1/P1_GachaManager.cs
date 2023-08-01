d271c935e863442bc01dfe05c4be752f3cc76ebc
jjc-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-08-01 10:29:02 +0900

    ガチャはできた

--- Assets/Scripts/Project1/P1_GachaManager.cs
+++ Assets/Scripts/Project1/P1_GachaManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class P1_GachaManager : MonoBehaviour
 {
@@ -9,10 +10,20 @@ public class P1_GachaManager : MonoBehaviour
 
     const int CHARACTER_NUM = 32;
 
+    GameObject gachaPerformInstance;
+    Image performImage;
+    Text performName;
+    Image performBack;
+
     // Start is called before the first frame update
     void Start()
     {
         gachaPickCharacterId = 0;
+        gachaPerformInstance = GameObject.Find("UIParts_Perform_Project1");
+        performImage = gachaPerformInstance.transform.Find("Image").GetComponent<Image>();
+        performName = gachaPerformInstance.transform.Find("Name").GetComponent<Text>();
+        performBack = gachaPerformInstance.transform.Find("Back").GetComponent<Image>();
+        gachaPerformInstance.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,10 +32,25 @@ public class P1_GachaManager : MonoBehaviour
         
     }
 
-    public void OnClick()
+    public void OnClickGachaButton()
     {
         gachaPickCharacterId = Random.Range(0, CHARACTER_NUM);
 
         Debug.Log(gachaPickCharacterId.ToString());
+
+        DrawPerform();
+    }
+
+    public void DrawPerform()
+    {
+        gachaPerformInstance.SetActive(true);
+        FixCharaManager.FixCharaData fixCharaData = UserApplication.fixCharaManager.GetFixCharaData(gachaPickCharacterId);
+        performImage.sprite = Resources.Load<Sprite>(fixCharaData.m_tachiePath);
+        performName.text = fixCharaData.m_name;
+    }
+
+    public void OnClickGachaPerformBack()
+    {
+        gachaPerformInstance.SetActive(false);
     }
 }
