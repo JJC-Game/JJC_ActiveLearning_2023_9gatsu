78193eb3b7583429ba25967a844d8deaebc3e7fc
JJC-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-08-18 12:22:20 +0900

    32番目に対応してなかった。

--- Etc/Laravel/views/play_chara_gacha.blade.php
+++ Etc/Laravel/views/play_chara_gacha.blade.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'>
-        <title>gacha play_chara_gacha</title>
-    </head>
-    <body>
-        <p>{{ $result_string }}</p>
-    </body>
-</html>
+{{ $result_string }}
